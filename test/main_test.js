db58d189b8aba0c19a376b6c0b0eb01a3fa00190
tylerakin2348 <tylerakinmwc@gmail.com>
2021-07-03 10:19:56 -0400

    adds line

--- test/main_test.js
+++ test/main_test.js
@@ -1,11 +1,11 @@
 // Test suite setup
- // - block of closely-related tests
-var expect = require('chai').expect;
+// - block of closely-related tests
+var expect = require("chai").expect;
 
- describe( 'Mocha', function() {
-    // Test spec (individual unit tests) here
+describe("Mocha", function() {
+  // Test spec (individual unit tests) here
 
-    it('should run our tests using npm', function(){
-        expect(true).to.be.ok;
-    });
- });
+  it("should run our tests using npm", function() {
+    expect(true).to.be.ok;
+  });
+});
